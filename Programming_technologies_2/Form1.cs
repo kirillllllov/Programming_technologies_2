207b18d743d5cb39563fbc5496db064acaaaf070
koptelov.kir <koptelov.kir@mail.ru>
2025-02-21 17:51:57 +0800

    Добавьте файлы проекта.

---
+++ Programming_technologies_2/Form1.cs
@@ -0,0 +1,45 @@
+﻿namespace Programming_technologies_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string result;
+            try
+            {
+                result = this.sentence.Text;
+                
+            }
+            catch (FormatException) // тип ошибки, которую перехватываем
+            {
+                return; // прерываем обработчик клика, если ввели какую-то ерунду
+            }
+            
+            MessageBox.Show("Вы ввели: " + result + "\n" + Logic.CalculateLetterPercentage(result));
+
+        }
+
+        public class Logic
+        {
+            public double percentage;
+            public static string CalculateLetterPercentage(string input)
+            {
+                // Подсчёт общего количества символов и количества букв
+                int totalChars = input.Length;
+                int letterCount = input.Count(char.IsLetter);
+
+                // Вычисление процента букв
+                double percentage = Math.Round(((double)letterCount / totalChars * 100), 2);
+                string output = $"Доля букв в предложении:  {percentage}%";
+                return output;
+
+            }
+        }
+
+    }
+}
